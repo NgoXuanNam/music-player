561b8722fc47bea7540619ee64797405dbce0152
NgoXuanNam <ngoxuannamit.vn@gmail.com>
2022-12-09 08:30:41 +0700

    new update songs

--- src/components/Section/Content/DetailSong.js
+++ src/components/Section/Content/DetailSong.js
@@ -4,7 +4,7 @@ export default function DetailSong() {
   const { song } = useContext(Songs);
 
   return (
-    <div className="detailSong col-span-1  p-3">
+    <div className="detailSong col-span-1  p-3 overflow-y-scroll">
       <div className="detailSong_heading ">
         <h3 className="text-[1.2rem] font-medium text-[#38bdf8]">
           Now playing
@@ -12,8 +12,8 @@ export default function DetailSong() {
         <p className="text-[1.6rem] text-[#ddd]">{song.name}</p>
       </div>
       <div className="detailSong_body w-full flex flex-col justify-center items-center mt-5">
-        <img src={song.links.images[0].url} alt="avatar" className="w-[70%]" />
-        <div className="detailSong_body-author flex w-[70%] items-center justify-between mt-5">
+        <img src={song.links.images[0].url} alt="avatar" className="w-[70%] " />
+        <div className="detailSong_body-author flex min-w-[70%] items-center justify-between mt-5">
           <img
             src={song.links.images[1].url}
             alt="avatar"
